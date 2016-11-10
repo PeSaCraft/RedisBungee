6a6c4561bf7a77688f5b999233330d6850276b09
FroznMine <froznmine@pesacraft.de>
2016-11-11 00:15:57 +0100

    Finished conversion of PlayerConnectionListener
    Also excluded everything category related to do this.

--- src/main/java/com/imaginarycode/minecraft/redisbungee/listener/PlayerConnectionListener.java
+++ src/main/java/com/imaginarycode/minecraft/redisbungee/listener/PlayerConnectionListener.java
@@ -4,22 +4,29 @@ import javax.annotation.Resource;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Component;
 
 import com.imaginarycode.minecraft.redisbungee.RedisBungee;
 import com.imaginarycode.minecraft.redisbungee.RedisBungeeCore;
 import com.imaginarycode.minecraft.redisbungee.RedisUtil;
+import com.imaginarycode.minecraft.redisbungee.manager.CachedDataManager;
 import com.imaginarycode.minecraft.redisbungee.manager.ServerManager;
 import com.imaginarycode.minecraft.redisbungee.util.RedisCallable;
 import com.imaginarycode.minecraft.redisbungee.util.uuid.UUIDTranslator;
 
 import de.pesacraft.shares.config.CustomRedisTemplate;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.event.ServerConnectedEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
@@ -28,6 +35,17 @@ import redis.clients.jedis.Pipeline;
 
 @Component
 public class PlayerConnectionListener implements Listener, InitializingBean {
+
+	private static final BaseComponent[] ALREADY_LOGGED_IN =
+			new ComponentBuilder("You are already logged on to this server.").color(ChatColor.RED)
+					.append("\n\nIt may help to try logging in again in a few minutes.\nIf this does not resolve your issue, please contact staff.")
+					.color(ChatColor.GRAY)
+					.create();
+	private static final BaseComponent[] ONLINE_MODE_RECONNECT =
+			new ComponentBuilder("Whoops! You need to reconnect.").color(ChatColor.RED)
+					.append("\n\nWe found someone online using your username. They were kicked and you may reconnect.\nIf this does not work, please contact staff.")
+					.color(ChatColor.GRAY)
+					.create();
 
 	@Autowired
 	private RedisBungee plugin;
@@ -41,11 +59,17 @@ public class PlayerConnectionListener implements Listener, InitializingBean {
 	@Resource(name = "redisTemplate")
 	private SetOperations<String, String> setOperations;
 
+	@Resource(name = "redisTemplate")
+	private HashOperations<String, String, String> hashOperations;
+
 	@Autowired
 	private UUIDTranslator uuidTranslator;
 
 	@Autowired
 	private RedisUtil redisUtil;
+
+	@Autowired
+	private CachedDataManager cachedDataManager;
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
@@ -91,22 +115,107 @@ public class PlayerConnectionListener implements Listener, InitializingBean {
 	@EventHandler
 	public void onPostLogin(final PostLoginEvent event) {
 		// Invalidate all entries related to this player, since they now lie.
-		invalidate(event.getPlayer().getUniqueId());
+		cachedDataManager.invalidate(event.getPlayer().getUniqueId());
 
 		plugin.getProxy().getScheduler().runAsync(plugin, () -> {
-			redisTemplate.convertAndSend("redisbungee-data", new DataManager.DataManagerMessage<>(
-					event.getPlayer().getUniqueId(), DataManager.DataManagerMessage.Action.JOIN,
-					new DataManager.LoginPayload(event.getPlayer().getAddress().getAddress()))));
+			redisTemplate.convertAndSend("redisbungee-data", new CachedDataManager.DataManagerMessage<>(
+					event.getPlayer().getUniqueId(), CachedDataManager.DataManagerMessage.Action.JOIN,
+					new CachedDataManager.LoginPayload(event.getPlayer().getAddress().getAddress())));
 		});
 	}
 
 	@EventHandler
 	public void onPlayerDisconnect(final PlayerDisconnectEvent event) {
 		// Invalidate all entries related to this player, since they now lie.
-		invalidate(event.getPlayer().getUniqueId());
+		cachedDataManager.invalidate(event.getPlayer().getUniqueId());
 
 		plugin.getProxy().getScheduler().runAsync(plugin, () -> {
 			redisUtil.cleanUpPlayer(event.getPlayer().getUniqueId().toString());
 		});
+	}
+
+	@EventHandler
+	public void onServerChange(final ServerConnectedEvent event) {
+		final ServerInfo currentServer = event.getPlayer().getServer() == null ? null : event.getPlayer().getServer().getInfo();
+		final ServerInfo newServer = event.getServer().getInfo();
+
+		if (currentServer == null) {
+			// joining network
+			final String newServerName = newServer.getName();
+			final String uuid = event.getPlayer().getUniqueId().toString();
+
+			plugin.getProxy().getScheduler().runAsync(plugin, () -> {
+				hashOperations.put("player:" + uuid, "server", newServerName);
+				setOperations.add("server:" + newServerName + ":usersOnline", uuid);
+
+//				jedis.sadd("category:" + newServer.getCategory().getName() + ":usersOnline", uuid);
+//
+//				int players;
+//				switch (newServer.getServerType()) {
+//				case DIRECT: case LOBBY:
+//					players = jedis.zincrby("category:" + newServer.getCategory().getName() + ":servers", 1, newServerName).intValue();
+//					newServer.getCategory().putServer(newServer, players);
+//					break;
+//				case GAME:
+//					String map = newServer.getMap();
+//					players = jedis.zincrby("category:" + newServer.getCategory().getName() + ":map:" + map, 1, newServerName).intValue();
+//					((LobbyGameCategoryInfo) newServer.getCategory()).putGameServer(newServer.getMap(), newServer, players);
+//					break;
+//				default:
+//					break;
+//				}
+				redisTemplate.convertAndSend("redisbungee-data", RedisBungeeCore.getGson().toJson(new CachedDataManager.DataManagerMessage<>(
+						event.getPlayer().getUniqueId(), CachedDataManager.DataManagerMessage.Action.SERVER_CHANGE,
+						new CachedDataManager.ServerChangePayload(newServerName, null))));
+			});
+		}
+		else {
+			// switching server
+			final String currentServerName = currentServer.getName();
+			final String newServerName = newServer.getName();
+			final String uuid = event.getPlayer().getUniqueId().toString();
+
+			plugin.getProxy().getScheduler().runAsync(plugin, () -> {
+				hashOperations.put("player:" + uuid, "server", newServerName);
+				setOperations.remove("server:" + currentServerName + ":usersOnline", uuid);
+				setOperations.add("server:" + newServerName + ":usersOnline", uuid);
+
+//				if (newServer.getCategory() != currentServer.getCategory()) {
+//					jedis.srem("category:" + currentServer.getCategory().getName() + ":usersOnline", uuid);
+//					jedis.sadd("category:" + newServer.getCategory().getName() + ":usersOnline", uuid);
+//				}
+//
+//				int players;
+//				switch (currentServer.getServerType()) {
+//				case DIRECT: case LOBBY:
+//					players = jedis.zincrby("category:" + currentServer.getCategory().getName() + ":servers", -1, currentServerName).intValue();
+//					currentServer.getCategory().putServer(currentServer, players);
+//					break;
+//				case GAME:
+//					String map = currentServer.getMap();
+//					players = jedis.zincrby("category:" + currentServer.getCategory().getName() + ":map:" + map, -1, currentServerName).intValue();
+//					((LobbyGameCategoryInfo) currentServer.getCategory()).putGameServer(currentServer.getMap(), currentServer, players);
+//					break;
+//				default:
+//					break;
+//				}
+//				switch (newServer.getServerType()) {
+//				case DIRECT: case LOBBY:
+//					players = jedis.zincrby("category:" + newServer.getCategory().getName() + ":servers", 1, newServerName).intValue();
+//					newServer.getCategory().putServer(newServer, players);
+//					break;
+//				case GAME:
+//					String map = newServer.getMap();
+//					players = jedis.zincrby("category:" + newServer.getCategory().getName() + ":map:" + map, 1, newServerName).intValue();
+//					((LobbyGameCategoryInfo) newServer.getCategory()).putGameServer(newServer.getMap(), newServer, players);
+//					break;
+//				default:
+//					break;
+//				}
+				redisTemplate.convertAndSend("redisbungee-data", RedisBungeeCore.getGson().toJson(new CachedDataManager.DataManagerMessage<>(
+						event.getPlayer().getUniqueId(), CachedDataManager.DataManagerMessage.Action.SERVER_CHANGE,
+						new CachedDataManager.ServerChangePayload(newServerName, currentServerName))));
+			});
+		}
 	}
 }
